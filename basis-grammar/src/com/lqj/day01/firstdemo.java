a3b559384700ee0337864315d8f04c7dfad7a29f
1635334137 <18877888233@163.com>
2021-11-22 23:12:05 +0800

    Java八种基本类型的信息

---
+++ basis-grammar/src/com/lqj/day01/firstdemo.java
@@ -0,0 +1,33 @@
+package com.lqj.day01;
+
+public class firstdemo {
+    public static void main(String[] args) {
+        System.out.println("基本类型：byte "+Byte.SIZE+"bit");
+        System.out.println("min_value="+Byte.MIN_VALUE);
+        System.out.println("max_value="+Byte.MAX_VALUE);
+
+        System.out.println("基本类型：short "+Short.SIZE+"bit");
+        System.out.println("min_value="+Short.MIN_VALUE);
+        System.out.println("max_value="+Short.MAX_VALUE);
+
+        System.out.println("基本类型：int "+Integer.SIZE+"bit");
+        System.out.println("min_value="+Integer.MIN_VALUE);
+        System.out.println("max_value="+Integer.MAX_VALUE);
+
+        System.out.println("基本类型：long "+Long.SIZE+"bit");
+        System.out.println("min_value="+Long.MIN_VALUE);
+        System.out.println("max_value="+Long.MAX_VALUE);
+
+        System.out.println("基本类型：float "+Float.SIZE+"bit");
+        System.out.println("min_value="+Float.MIN_VALUE);
+        System.out.println("max_value="+Float.MAX_VALUE);
+
+        System.out.println("基本类型：double "+Double.SIZE+"bit");
+        System.out.println("min_value="+Double.MIN_VALUE);
+        System.out.println("max_value="+Double.MAX_VALUE);
+
+        System.out.println("基本类型：char "+Character.SIZE+"bit");
+        System.out.println("min_value="+(int)Character.MIN_VALUE);
+        System.out.println("max_value="+(int)Character.MAX_VALUE);
+    }
+}
