9ec3b158ef9dea6168e01a3eeb8176c813925371
1635334137 <18877888233@163.com>
2022-01-27 02:59:24 +0800

    Java日志

--- elasticsearch-demo/src/main/java/org/example/es/ESClient.java
+++ elasticsearch-demo/src/main/java/org/example/es/ESClient.java
@@ -2,7 +2,7 @@ package org.example.es;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
-import org.apache.http.client.methods.HttpOptions;
+
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
@@ -21,8 +21,9 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetIndexResponse;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.mapper.ObjectMapper;
 import org.example.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -37,17 +38,19 @@ public class ESClient {
     private static final int ES_SERVER_PORT = 9200;
     private static final String ES_CONNECT_WAY = "http";
 
+   static Logger logger = LoggerFactory.getLogger(ESClient.class);
+
     public static void main(String[] args) throws IOException {
         //创建ES客户端连接
         RestHighLevelClient esClient = new RestHighLevelClient(RestClient.builder(new HttpHost(ES_SERVER_ADDRESS, ES_SERVER_PORT, ES_CONNECT_WAY)));
-
+       // logger.info("sfsdfsdfsdfsdf");
        // createIndex(esClient,"user");
         //selectIndex(esClient,"user");
         //deleteIndex(esClient,"user");
         //insertDocument(esClient,"user");
-        //updateDocument(esClient,"user");
+        updateDocument(esClient,"user");
         //selectDocument(esClient,"user");
-        deleteDocument(esClient,"user");
+        //deleteDocument(esClient,"user");
         //关闭连接
         esClient.close();
     }
@@ -131,7 +134,7 @@ public class ESClient {
         req.index(indexName).id("1001");
 
         //修改数据
-        req.doc(XContentType.JSON,"sex","女1");
+        req.doc(XContentType.JSON,"sex","女6");
 
         UpdateResponse response = client.update(req,RequestOptions.DEFAULT);
 
--- elasticsearch-demo/src/test/java/org/example/AppTest.java
+++ elasticsearch-demo/src/test/java/org/example/AppTest.java
@@ -3,6 +3,8 @@ package org.example;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
@@ -13,8 +15,8 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+       // Logger logger = LoggerFactory.getLogger(AppTest.class);
+       // logger.info("aasdf");
     }
 }
